478b47b50b6cd7f03f0b658091b3b592d075194f
mohamedBenHachme <benhachmemohamed@gmail.com>
2019-10-04 12:17:43 +0100

    fixing the list of keywords to prevent highlighting errors

--- textEditor/Keywords.java
+++ textEditor/Keywords.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Keywords {
 	private String[] languages = {".java"};
-	private String[] javaKeywords = {" abstract ", " assert ", " boolean ",
-            "break ", " byte ", " case ", " catch ", " char ", " class ", " const ",
-            "continue", "default", "do", "double", "else", " extends ", " false ",
-            "final", " finally ", " float ", " for ", " goto ", " if ", " implements ",
-            "import ", " instanceof ", " int ", " System ",
-            "new", "null", "package", "private", "protected", "public", " interface ",
-            "long ", " native ", " return ", " short ", "static ", "strict ", "super ", " switch ",
-            "synchronized", "this", "throw", "throws ", "transient ", "true ",
-            "try ", "void ", "volatile ", "while ", "String "};
+	private String[] javaKeywords = {"abstract","assert","boolean",
+	           "break","byte","case","catch","char","class","const",
+	           "continue","default","do","double","else","extends","false",
+	           "final","finally","float","for","goto","if","implements",
+	           "import","instanceof","int","system",
+	           "new","null","package","private","protected","public","interface",
+	           "long","native","return","short","static","strict","super","switch",
+	           "synchronized","this","throw","throws","transient","true",
+	           "try","void","volatile","while","string"};
 	private String[] brackets = {"(","{"};
 	private String[] bracketsCompletion = {")","}"};
 	public String[] getLanguages(){
